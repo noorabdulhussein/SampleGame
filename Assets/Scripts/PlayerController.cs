433fc3bd9d3c56af61a829c1a2ac6f4d6b84ebc3
Noor Abdulhussein <noor_abdulhussein03@outlook.com>
2024-10-18 13:15:26 +0100

    canvas and script changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
@@ -9,11 +11,16 @@ public class PlayerController : MonoBehaviour
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI WinText;
 
-    private void Start()
+    void Start()
     {
         count = 0;
-        
+        WinText.text = " ";
+        SetCountText();
+
     }
 
     void OnMove(InputValue value)
@@ -29,13 +36,21 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-       if (other.gameObject.tag == "PickUp")
+        if (other.gameObject.tag == "PickUp")
         {
-           other.gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
             count++;
+            SetCountText();
         }
-       
+
     }
 
-
+    private void SetCountText()
+    {
+        ScoreText.text = " Score : " + count.ToString();
+        if (count >= numPickups)
+        {
+            WinText.text = " You win ! ";
+        }
+    }
 }
