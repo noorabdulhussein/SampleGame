2bd7b72b72816bcd197ac17f7e51f91463d75b85
Noor Abdulhussein <noor_abdulhussein03@outlook.com>
2024-10-18 13:19:55 +0100

    Revert "canvas and script changes"
    This reverts commit 433fc3bd9d3c56af61a829c1a2ac6f4d6b84ebc3.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
@@ -11,16 +9,11 @@ public class PlayerController : MonoBehaviour
     public Vector2 moveValue;
     public float speed;
     private int count;
-    private int numPickups = 5;
-    public TextMeshProUGUI ScoreText;
-    public TextMeshProUGUI WinText;
 
-    void Start()
+    private void Start()
     {
         count = 0;
-        WinText.text = " ";
-        SetCountText();
-
+        
     }
 
     void OnMove(InputValue value)
@@ -36,21 +29,13 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "PickUp")
+       if (other.gameObject.tag == "PickUp")
         {
-            other.gameObject.SetActive(false);
+           other.gameObject.SetActive(false);
             count++;
-            SetCountText();
         }
-
+       
     }
 
-    private void SetCountText()
-    {
-        ScoreText.text = " Score : " + count.ToString();
-        if (count >= numPickups)
-        {
-            WinText.text = " You win ! ";
-        }
-    }
+
 }
